b108b976ed067bb2f6a22d0acfb2f4f183f0acb1
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-06-12 23:24:58 +0430

    Login.js imported in App.js. Also react-router-dom imported becuase i needed to define Route(s) and use Redirect to Specify the path for eact login and sign up pages. and also use Switch that when i want change the page, the browser did not reload

--- registration-form/src/App.js
+++ registration-form/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
 
 // components
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 const App = () => {
   return (
-    <div>
-      <SignUp />
-    </div>
+    <Switch>
+      <Route path="/login" component={Login} />      
+      <Route path="/signup" component={SignUp} />
+      <Redirect from="/" to="signup"/>      
+    </Switch>
   );
 };
 
