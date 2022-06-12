897e8acdb967187a183b73b009857ba828b00657
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-06-12 12:56:17 +0430

    Primary structure of sign up form created. I use useState hook that have main information of the form in an object

--- registration-form/src/components/SignUp.js
+++ registration-form/src/components/SignUp.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState} from 'react';
 import styles from "./SignUp.module.css";
 
 const SignUp = () => {
+
+    const [data, setData] = useState({
+        name : "",
+        email : "",
+        password : "",
+        confirmPassword : "",
+        isAccepted : false,
+    })
+
     return (
-        <div>
-            
+        <div clasName={styles.container}>
+            <from className={styles.formContainer}>
+                <h2 className={styles.header}>Sign Up</h2>
+                <div>
+                    <label>Name</label>
+                    <input
+                    type="text"
+                    name="name"
+                    value={data.name}>
+                    </input>
+                </div>
+
+                <div>
+                    <label>Email</label>
+                    <input
+                    type="text"
+                    name="email"
+                    value={data.email}>
+                    </input>
+                </div>
+
+                <div>
+                    <label>Password</label>
+                    <input
+                    type="password"
+                    name="password"
+                    value={data.password}>
+                    </input>
+                </div>
+
+                <div>
+                    <label>Confirm Password</label>
+                    <input
+                    type="password"
+                    name="confirmPassword"
+                    value={data.confirmPassword}>
+                    </input>
+                </div>
+
+                <div>
+                    <label>Name</label>
+                    <input
+                    type="text"
+                    name="name"
+                    value={data.name}>
+                    </input>
+                </div>
+            </from>
         </div>
     );
 };
