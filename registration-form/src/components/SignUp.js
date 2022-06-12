d4cc15bdc1101d27e49ef8424c638f927873f550
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-06-12 16:51:31 +0430

    focusHandler added to show errors when client touched the inputs and submitHandler added that when client did not fill the inputs but click the submit, shows the errors to the client

--- registration-form/src/components/SignUp.js
+++ registration-form/src/components/SignUp.js
@@ -14,11 +14,12 @@ const SignUp = () => {
     });
 
     const [errors, setErrors] = useState({});
+    const [touched, setTouched] = useState({});
 
     useEffect(() => {
         setErrors(validate(data))
         console.log(errors)
-    }, [data])
+    }, [data, touched])
 
 
 
@@ -26,14 +27,33 @@ const SignUp = () => {
         if(event.target.name === "isAccepted") {
             setData({...data, [event.target.name] : event.target.checked})
         } else {
-            setData({...data, [event.target.name]: event.target.value})
+            setData({...data, [event.target.name] : event.target.value})
         }
         console.log(data)
     }
 
+    const focusHandler =(event) => {
+        setTouched({...data, [event.target.name] : true})
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if (!Object.keys(errors).length) {
+
+        }else {
+            setTouched({
+                name: true,
+                email: true,
+                password: true,
+                confirmPassword: true,
+                isAccepted: true,
+            })
+        }
+    }
+
     return (
         <div className={styles.container}>
-            <form className={styles.formContainer}>
+            <form className={styles.formContainer} onSubmit={submitHandler}>
                 <h2 className={styles.header}>Sign Up</h2>
                 <div>
                     <label>Name</label>
@@ -41,8 +61,10 @@ const SignUp = () => {
                     type="text"
                     name="name"
                     value={data.name}
-                    onChange={changeHandler}>
+                    onChange={changeHandler}
+                    onFocus={focusHandler}>
                     </input>
+                    {errors.name && touched.name && <span>{errors.name}</span>}
                 </div>
 
                 <div>
@@ -51,8 +73,10 @@ const SignUp = () => {
                     type="text"
                     name="email"
                     value={data.email}
-                    onChange={changeHandler}>
+                    onChange={changeHandler}
+                    onFocus={focusHandler}>
                     </input>
+                    {errors.email && touched.email && <span>{errors.email}</span>}
                 </div>
 
                 <div>
@@ -61,8 +85,10 @@ const SignUp = () => {
                     type="password"
                     name="password"
                     value={data.password}
-                    onChange={changeHandler}>
+                    onChange={changeHandler}
+                    onFocus={focusHandler}>
                     </input>
+                    {errors.password && touched.password && <span>{errors.password}</span>}
                 </div>
 
                 <div>
@@ -71,8 +97,10 @@ const SignUp = () => {
                     type="password"
                     name="confirmPassword"
                     value={data.confirmPassword}
-                    onChange={changeHandler}>
+                    onChange={changeHandler}
+                    onFocus={focusHandler}>
                     </input>
+                    {errors.confirmPassword && touched.confirmPassword && <span>{errors.confirmPassword}</span>}
                 </div>
 
                 <div>
@@ -81,8 +109,10 @@ const SignUp = () => {
                     type="checkbox"
                     name="isAccepted"
                     value={data.isAccepted}
-                    onChange={changeHandler}>
+                    onChange={changeHandler}
+                    onFocus={focusHandler}>
                     </input>
+                    {errors.name && touched.isAccepted && <span>{errors.isAccepted}</span>}
                 </div>
                 <div>
                     <a href="#">Login</a>
